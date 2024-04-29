ac4a800db69653f47d22dc349378de33fe6152af
khdoba <kholboevdostonbek@gmail.com>
2024-04-29 17:44:56 +0500

    API_URL from env

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 
-const API_URL = 'https://www.u-barber.uz/api';
 const API_KEY = process.env.REACT_APP_API_KEY;
-const API_URL2 = process.env.REACT_APP_API_URL;
-export { API_URL, API_KEY, API_URL2};
+const API_URL = process.env.REACT_APP_API_URL;
+export { API_URL, API_KEY};

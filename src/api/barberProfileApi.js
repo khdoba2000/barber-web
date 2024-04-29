802704799447bc969d708cfe871c64c8d1808884
khdoba <kholboevdostonbek@gmail.com>
2024-04-29 17:39:32 +0500

    logging eenv variable

--- src/api/barberProfileApi.js
+++ src/api/barberProfileApi.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 import {generateHMAC, getCurrentEpochTime} from '../util';
 
-import { API_URL } from '../config';
+import { API_URL, API_URL2 } from '../config';
 
 
 const fetchBarberProfile = async (id) => {
     try {
+        console.log('API_URL2:', API_URL2);
         const response = await axios.get(`${API_URL}/clients/barbers/${id}/profile/`, { 
             headers: { "Authorization-HMAC" :  generateHMAC(null, getCurrentEpochTime()), "Timestamp" : getCurrentEpochTime()},
         });
--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 
 const API_URL = 'https://www.u-barber.uz/api';
 const API_KEY = process.env.REACT_APP_API_KEY;
-export { API_URL, API_KEY};
+const API_URL2 = process.env.REACT_APP_API_URL;
+export { API_URL, API_KEY, API_URL2};
